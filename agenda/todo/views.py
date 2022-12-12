1f13d6afa2bfb9d4998186145b5a38e0ba881435
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-12 11:06:08 -0500

    Asignando rutas a [todo]

--- agenda/todo/views.py
+++ agenda/todo/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 def index(request):
+  todos = Todo.objects.filter(title__contains=request.GET.get('search', ''))
+  context = {
+    'todos': todos
+  }
+  return render(request, 'todo/index.html', context)
+
+def view(request, id):
+  return render(request, 'todo/index.html', {})
+
+def edit(request, id):
+  return render(request, 'todo/index.html', {})
+
+def create(request):
+  return render(request, 'todo/index.html', {})
+
+def delete(request, id):
   return render(request, 'todo/index.html', {})
