6b33a72d2fa2043b9f59f28b8e42867f08094fc5
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-12 08:16:11 -0500

    Creando vistas [ver, editar (contact_view, contact_edit)]

--- agenda/contact/urls.py
+++ agenda/contact/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='contact'),
-  path('view/<int:id>', views.view, name='contact_view')
+  path('view/<int:id>', views.view, name='contact_view'),
+  path('edit/<int:id>', views.edit, name='contact_edit'),
 ]
