f6e5ae793ef108d1876b94783f4b4fa9db511b58
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-12 11:25:06 -0500

    Corrigiendo errores en rutas[Todo]

--- agenda/todo/urls.py
+++ agenda/todo/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
   path('view/<int:id>', views.view, name='todo_view'),
   path('edit/<int:id>', views.edit, name='todo_edit'),
   path('create/', views.create, name='todo_create'),
-  path('delete/', views.delete, name='todo_delete'),
+  path('delete<int:id>/', views.delete, name='todo_delete'),
 ]
